cbb50fb0143cb343af87e0c5bd0ff762ca721cfe
kaisertill <t.kaiser96@outlook.de>
2018-11-17 12:40:26 +0100

    Convertierungs Problem

--- Fasenrechner/Form1.Designer.cs
+++ Fasenrechner/Form1.Designer.cs
@@ -62,6 +62,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -83,7 +84,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "Rechnen";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -92,7 +93,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
+            this.button2.Text = "Reset";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -101,27 +102,27 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(328, 50);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
+            this.label1.Text = "Winkel Alpha";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(328, 76);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(40, 13);
             this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
+            this.label2.Text = "Z-Wert";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(328, 102);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(69, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = "label3";
+            this.label3.Text = "X-Wert  (abs)";
             // 
             // textBox4
             // 
@@ -135,14 +136,14 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(328, 128);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(66, 13);
             this.label4.TabIndex = 10;
-            this.label4.Text = "label4";
+            this.label4.Text = "X-Wert  (inc)";
             // 
             // textBox5
             // 
             this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(588, 125);
+            this.textBox5.Location = new System.Drawing.Point(579, 47);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 14;
@@ -150,7 +151,7 @@
             // textBox6
             // 
             this.textBox6.Enabled = false;
-            this.textBox6.Location = new System.Drawing.Point(588, 99);
+            this.textBox6.Location = new System.Drawing.Point(579, 73);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 20);
             this.textBox6.TabIndex = 13;
@@ -158,7 +159,7 @@
             // textBox7
             // 
             this.textBox7.Enabled = false;
-            this.textBox7.Location = new System.Drawing.Point(588, 73);
+            this.textBox7.Location = new System.Drawing.Point(579, 99);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(100, 20);
             this.textBox7.TabIndex = 12;
@@ -166,7 +167,7 @@
             // textBox8
             // 
             this.textBox8.Enabled = false;
-            this.textBox8.Location = new System.Drawing.Point(588, 47);
+            this.textBox8.Location = new System.Drawing.Point(579, 125);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(100, 20);
             this.textBox8.TabIndex = 11;
@@ -176,18 +177,18 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(435, 31);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(46, 13);
             this.label5.TabIndex = 15;
-            this.label5.Text = "label5";
+            this.label5.Text = "Eingabe";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(585, 31);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(48, 13);
             this.label6.TabIndex = 16;
-            this.label6.Text = "label6";
+            this.label6.Text = "Ergebnis";
             // 
             // Form1
             // 
--- Fasenrechner/Form1.cs
+++ Fasenrechner/Form1.cs
@@ -20,12 +20,38 @@ namespace Fasenrechner
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = (null);
+            textBox2.Text = (null);
+            textBox3.Text = (null);
+            textBox4.Text = (null);
+            textBox5.Text = (null);
+            textBox6.Text = (null);
+            textBox7.Text = (null);
+            textBox8.Text = (null);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            double angle;
+            double xabs;
+            double xinc;
+            double z;
+            angle = Convert.ToDouble(textBox1.Text);
+            z = Convert.ToDouble(textBox2.Text);
+            xabs = Convert.ToDouble(textBox3.Text);
+            xinc = Convert.ToDouble(textBox4.Text);
+
+            if(textBox1.Text != null && textBox2.Text != null) 
+            {
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
+ 
+
 }
